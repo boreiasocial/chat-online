3fdff2feee8507a454ad92aabd4135f6170b5468
boreiasocial <boreiasocial@gmail.com>
2026-07-24 14:13:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,34 +6,43 @@ app.config["SECRET_KEY"] = "chat-online"
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-usuarios = set()
+# Guarda os usuários conectados
+usuarios = {}
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @socketio.on("entrar")
 def entrar(nome):
-    usuarios.add(nome)
+    usuarios[request.sid] = nome
+
     emit("mensagem", {
         "autor": "Sistema",
-        "texto": f"{nome} entrou no chat!"
+        "texto": f"{nome} entrou no chat."
     }, broadcast=True)
 
-    emit("usuarios", list(usuarios), broadcast=True)
+    emit("usuarios", list(usuarios.values()), broadcast=True)
+
 
 @socketio.on("mensagem")
 def mensagem(data):
     emit("mensagem", data, broadcast=True)
 
+
 @socketio.on("disconnect")
 def sair():
-    # Nesta primeira versão não removemos o usuário pelo nome,
-    # isso vamos melhorar depois.
-    emit("mensagem", {
-        "autor": "Sistema",
-        "texto": "Um usuário saiu."
-    }, broadcast=True)
+    nome = usuarios.pop(request.sid, None)
+
+    if nome:
+        emit("mensagem", {
+            "autor": "Sistema",
+            "texto": f"{nome} saiu do chat."
+        }, broadcast=True)
+
+        emit("usuarios", list(usuarios.values()), broadcast=True)
+
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=10000)
